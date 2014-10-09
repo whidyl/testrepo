8d9b932e6ee9dd0dab01ebf02450e92145955335
Dylan Whitehurst <whidyldev@gmail.com>
2014-10-09 13:21:12 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,27 @@
-class test:
-  def __init__(x,y):
-    testx = x
-    testy = y
-  def testfunc(text):
-    return text
+import math
+
+class Vector3:
+  def __init__(self, x = 0,y = 0,z = 0):
+    self.x = x
+    self.y = y
+    self.z = z
+  def add(v):
+    self.x += v.x
+    self.y += v.y
+    self.z += v.z
+  def sub(v):
+    self.x -= v.x
+    self.y -= v.y
+    self.z -= v.z
+  def mult(n):
+    self.x *= n
+    self.y *= n
+    self.z *= n
+  def div(n):
+    self.x /= n
+    self.y /= n
+    self.z /= n
+  def mag():
+    return math.sqrt(self.x * self.x + self.y * self.y)
+  def normalize():
+    self.div(self.mag)
